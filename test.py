778002396467e72e0cea65efc512b9a2c1e4175a
JohnRyden <john.ryden@tietoevry.com>
2021-07-22 23:01:41 +0200

    Funkar snabbare nu

--- test.py
+++ test.py
@@ -13,16 +13,20 @@ class CellAutomata:
 
         self.myGrid = PyGrid.PGGrid(self.rows,self.cols,800,600)
         self.myGrid.tile_padding=0
+        self.myGrid.nogrid = False
+        self.myGrid.draw_border = False
 
-        self.pause_step = False
+        self.pause_step = True
 
     def remove_cell(self, pos):
-        self.myGrid.remove_tile(pos[0],pos[1])
-        self._filled_cells.remove(pos)
+        if pos in self._filled_cells:
+            self.myGrid.remove_tile(pos[0],pos[1])
+            self._filled_cells.remove(pos)
 
     def add_cell(self, pos):
-        self.myGrid.set_tile(pos[0],pos[1])
-        self._filled_cells.append(pos)
+        if pos not in self._filled_cells:
+            self.myGrid.set_tile(pos[0],pos[1])
+            self._filled_cells.append(pos)
 
     def update(self, new_cells):
         for c in new_cells:
@@ -32,31 +36,43 @@ class CellAutomata:
             else:
                 self.remove_cell(pos)
 
+    def count_neighbours(self, pos, recurse):
+        neighbour_count = 0
+        row,col = pos
+        newborns = []
+        for i in range(-1,2):
+            for j in range(-1,2):
+                if row + i < 0 or row + i >= self.rows:
+                    continue
+                if col + j < 0 or col + j >= self.cols:
+                    continue
+                if i == 0 and j == 0:
+                    continue
+
+                row_i = row + i
+                col_j = col + j
+                if recurse:
+                    surrounding_count, _ = self.count_neighbours((row_i,col_j), recurse=False)
+                    if surrounding_count == 3 and (row_i,col_j) not in self._filled_cells:
+                        if (row_i,col_j) not in newborns:
+                            newborns.append((row_i,col_j))
+                if (row_i, col_j) in self._filled_cells:
+                    neighbour_count += 1
+        return neighbour_count, newborns
+
     def step(self):
         # new cell = exactly 3 neighbours
         # cell dies = fewer than 2 neighbours or more than 3 neighbours
 
         new_cells = []
-        for row in range(0,self.rows):
-            for col in range(0,self.cols):
-                neighbour_count = 0
-                for i in range(-1,2):
-                    for j in range(-1,2):
-                        if row + i < 0 or row + i >= self.rows:
-                            continue
-                        if col + j < 0 or col + j >= self.cols:
-                            continue
-                        if i == 0 and j == 0:
-                            continue
-                        if (row + i, col + j) in self._filled_cells:
-                            neighbour_count += 1
-                current_state = (row,col) in self._filled_cells
-                if not current_state and neighbour_count == 3:
-                    new_cells.append(((row,col),True))
-                if current_state:
-                    if neighbour_count < 2 or neighbour_count > 3:
-                        new_cells.append(((row,col),False))
+        for c in self._filled_cells:
+            neighbour_count,newborn_cells = self.count_neighbours(c,recurse=True)
+            if neighbour_count < 2 or neighbour_count > 3:
+                new_cells.append((c, False))
+            for newbie in newborn_cells:
+                new_cells.append((newbie, True))
         self.update(new_cells)
+
 
     def handleInteraction(self):
         clicked_tile = self.myGrid.get_clicked_tile()
@@ -68,9 +84,9 @@ class CellAutomata:
                 self.last_interaction = datetime.now()
                 if self.pause_step == True:
                     self.pause_step = False
-                    print("unpausing")
+                    self.myGrid.draw_border = True
                 else:
-                    print("pausing")
+                    self.myGrid.draw_border = False
                     self.pause_step = True
 
 
@@ -92,7 +108,7 @@ class CellAutomata:
         while running:
             running = self.myGrid.update_drawing()
             self.handleInteraction()
-            if count == 1:
+            if count == 10:
                 if not self.pause_step:
                     self.step()
                 count = 0
